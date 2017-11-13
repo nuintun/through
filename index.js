afff24707c6072fb155b686a62e77c5dd5284f59
nuintun <nuintun@qq.com>
2017-11-13 09:33:59 +0800

    :hammer: use es6

--- index.js
+++ index.js
@@ -1,89 +1,72 @@
-/*!
- * through
- * Version: 0.0.1
- * Date: 2017/05/19
- * https://github.com/nuintun/through
- * https://github.com/rvagg/through2
- *
- * This is licensed under the MIT License (MIT).
- * For details, see: https://github.com/nuintun/through/blob/master/LICENSE
+/**
+ * @module index
+ * @license MIT
+ * @version 2017/11/13
  */
 
 'use strict';
 
-var Stream = require('readable-stream');
-var Transform = Stream.Transform;
+const Stream = require('stream');
+const Transform = Stream.Transform;
 
-var undef = void(0);
-var toString = Object.prototype.toString;
+const undef = void(0);
+const toString = Object.prototype.toString;
 
 /**
- * Is function
- *
+ * @function isFunction
  * @param {any} value
- * @returns {Boolean}
+ * @returns {boolean}
  */
 function isFunction(value) {
   return toString.call(value) === '[object Function]';
 }
 
 /**
- * DestroyableTransform
- *
- * @param options
- * @constructor
- */
-function DestroyableTransform(options) {
-  var context = this;
-
-  Transform.call(context, options);
-
-  // Destroyed flag
-  context._destroyed = false;
-}
-
-// extend
-DestroyableTransform.prototype = Object.create(Transform.prototype, { constructor: { value: DestroyableTransform } });
-
-/**
- * destroy
- *
- * @param error
- */
-DestroyableTransform.prototype.destroy = function(error) {
-  var context = this;
-
-  if (context._destroyed) return;
-
-  context._destroyed = true;
-
-  process.nextTick(function() {
-    if (error) context.emit('error', error);
-
-    context.emit('close');
-  });
-};
-
-/**
- * A noop _transform function
- *
- * @param chunk
- * @param encoding
- * @param next
+ * @function noop
+ * @description A noop _transform function
+ * @param {any} chunk
+ * @param {string} encoding
+ * @param {Function} next
  */
 function noop(chunk, encoding, next) {
   next(null, chunk);
 }
 
 /**
- * Create a new export function, used by both the main export,
- * contains common logic for dealing with arguments
- *
- * @param construct
+ * @class DestroyableTransform
+ */
+class DestroyableTransform extends Transform {
+  /**
+   * @constructor
+   * @param {Object} options
+   */
+  constructor(options) {
+    this._destroyed = false;
+  }
+
+  destroy(error) {
+    if (this._destroyed) return;
+
+    this._destroyed = true;
+
+    process.nextTick(() => {
+      if (error) this.emit('error', error);
+
+      this.emit('close');
+    });
+  }
+}
+
+/**
+ * @function through
+ * @description Create a new export function,
+ *  used by both the main export,
+ *  contains common logic for dealing with arguments
+ * @param {Function} construct
  * @returns {Function}
  */
 function through(construct) {
-  return function(options, transform, flush) {
+  return (options, transform, flush) => {
     if (isFunction(options)) {
       flush = transform;
       transform = options;
@@ -98,9 +81,13 @@ function through(construct) {
 }
 
 /**
- * Exports module
+ * @function through
+ * @param {Object} options
+ * @param {Function} transform
+ * @param {Function} flush
+ * @returns {Transform}
  */
-module.exports = through(function(options, transform, flush) {
+module.exports = through((options, transform, flush) => {
   if (options.objectMode === undef) {
     options.objectMode = true;
   }
@@ -109,7 +96,7 @@ module.exports = through(function(options, transform, flush) {
     options.highWaterMark = 16;
   }
 
-  var stream = new DestroyableTransform(options);
+  const stream = new DestroyableTransform(options);
 
   stream._transform = transform;
 
